8c659201851c2c472c3d6fee2a4eb5f6cb843de6
Hariesh <93585446+ZenShirogan@users.noreply.github.com>
2023-05-22 13:10:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Uncontrolled from './Uncontrolled';
+import Controlled from './Controlled';
+function App() {
+  return (
+    // <Uncontrolled />
+    <Controlled />
+
+  );
+}
+
+export default App;
